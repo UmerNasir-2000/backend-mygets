92f4303df1c4cedc16de09e06ae0ecf34b2a655a
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2023-12-05 21:23:20 +0500

    [FEAT] Added Register Endpoint :muscle:

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 
-const { requireUser, createUser } = require("./auth")
+const { requireUser, createUser, createAccessToken, fetchUserMetadataByEmail } = require("./auth")
 
 const app = express()
 
@@ -12,6 +12,35 @@ app.use(cors())
 
 app.get("/whoami", requireUser, (req, res) => {
   res.json({ userId: req.user.userId })
+})
+
+app.post(`/create-user`, async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+
+    const existingUser = await fetchUserMetadataByEmail(email);
+
+    if (existingUser) {
+      return res.status(409).json({ message: `User already exists` })
+    }
+    const user = await createUser({
+      email,
+      password,
+      sendEmailToConfirmEmailAddress: false,
+      emailConfirmed: true,
+    })
+    console.log("user", user)
+    const token = await createAccessToken({
+      userId: user.userId,
+      durationInMinutes: 60,
+    })
+    
+    return res.status(201).json({ userId: user.userId, token })
+  } catch (error) {
+    console.log("error", error)
+    return res.status(500).json({ message: error.message })
+  }
 })
 
 app.get("/", (_, res) => res.status(200).json({ message: `Hello, World!!!` }))
