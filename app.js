85dba46f043568dd8652d16fae8176137c711c69
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2023-12-06 23:08:44 +0500

    [FEAT] Added Invite Endpoint

--- app.js
+++ app.js
@@ -9,28 +9,28 @@ const {
   createAccessToken,
   fetchUserMetadataByEmail,
   createOrg,
-  addUserToOrg
+  addUserToOrg,
+  fetchUsersByQuery,
+  inviteUserToOrg,
 } = require("./auth")
 
 const app = express()
 
-var whitelist = ['http://localhost:3000', 'https://website-mygets.vercel.app/']
+// var whitelist = ['http://localhost:3000', 'https://website-mygets.vercel.app/', `*`]
 
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
- 
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
-
-// app.options('*', cors())
 
 app.get("/whoami", requireUser, (req, res) => {
   res.json({ userId: req.user.userId })
@@ -43,10 +43,32 @@ app.get("/list-organizations", requireUser, async (req, res) => {
 
 app.post("/create-organization", requireUser, async (req, res) => {
   const organization = await createOrg({ name: req.body.name })
-  const orgResponse = await addUserToOrg({ orgId: organization.orgId, userId: req.user.userId, role: `Admin` })
+  const orgResponse = await addUserToOrg({
+    orgId: organization.orgId,
+    userId: req.user.userId,
+    role: `Admin`,
+  })
   return res.status(201).json({ success: true })
 })
 
+app.post("/invite", requireUser, async (req, res) => {
+  console.log('req.body', req.body)
+  const users = await fetchUsersByQuery({ emailOrUsername: req.body.email })
+
+  console.log('users', users)
+  if (users.length === 0)
+    return res.status(404).json({ message: `User not found` })
+
+  const invitee = await inviteUserToOrg({
+    email: req.body.email,
+    orgId: req.body.orgId,
+    role: req.body.role,
+  })
+
+  console.log('invitee', invitee)
+
+  return res.status(201).json({ invitee })
+})
 
 app.post(`/create-user`, async (req, res) => {
   const { email, password } = req.body
